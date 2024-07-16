f3110777f5cf135931ebf6fed3848b1a86eb208d
siva_sankaran96 <sivasankaran003@hotmail.com>
2024-07-16 11:48:59 +0530

    'mas'

--- SqlServerProject/Person/Tables/Password.sql
+++ SqlServerProject/Person/Tables/Password.sql
@@ -2,7 +2,7 @@
     [BusinessEntityID] INT              NOT NULL,
     [PasswordHash]     VARCHAR (128)     NULL,
     [PasswordSalt]     VARCHAR (10)      NULL,
-
+    [master] varchar(max),
     [rowguid]          UNIQUEIDENTIFIER CONSTRAINT [DF_Password_rowguid] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
     [ModifiedDate]     DATETIME         CONSTRAINT [DF_Password_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_Password_BusinessEntityID] PRIMARY KEY CLUSTERED ([BusinessEntityID] ASC),
