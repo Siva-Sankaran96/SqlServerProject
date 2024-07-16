b6c8391d4115db6696558fdebc48941e68ef9db7
siva_sankaran96 <sivasankaran003@hotmail.com>
2024-07-16 11:19:24 +0530

    'test2'

--- SqlServerProject/Person/Tables/Password.sql
+++ SqlServerProject/Person/Tables/Password.sql
@@ -1,7 +1,8 @@
 ﻿CREATE TABLE [Person].[Password] (
     [BusinessEntityID] INT              NOT NULL,
     [PasswordHash]     VARCHAR (128)    NOT NULL,
-    [PasswordSalt]     VARCHAR (10)     NOT NULL,
+    [PasswordSalt]     VARCHAR (10)      NULL,
+
     [rowguid]          UNIQUEIDENTIFIER CONSTRAINT [DF_Password_rowguid] DEFAULT (newid()) ROWGUIDCOL NOT NULL,
     [ModifiedDate]     DATETIME         CONSTRAINT [DF_Password_ModifiedDate] DEFAULT (getdate()) NOT NULL,
     CONSTRAINT [PK_Password_BusinessEntityID] PRIMARY KEY CLUSTERED ([BusinessEntityID] ASC),
