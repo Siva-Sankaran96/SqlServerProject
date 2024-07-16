4e0739f2180da968a692f85bad2e9f9d51c52afd
Siva-Sankaran96 <sivasankaran003@hotmail.com>
2024-07-16 12:26:30 +0530

    Update AWBuildVersion.sql

--- SqlServerProject/dbo/Tables/AWBuildVersion.sql
+++ SqlServerProject/dbo/Tables/AWBuildVersion.sql
@@ -10,7 +10,8 @@ GO
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Version number of the database in 9.yy.mm.dd.00 format.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'AWBuildVersion', @level2type = N'COLUMN', @level2name = N'Database Version';
 
-
+GO
+    GO
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Date and time the record was last updated.', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = N'AWBuildVersion', @level2type = N'COLUMN', @level2name = N'ModifiedDate';
 
